492cf64cf98520a5ca3edcbab78efa44ce2457eb
Azim <ammtkrmv@gmail.com>
2022-11-18 20:32:24 +0600

    Feature: Added entities

---
+++ StudentManager.Model/Entities/Person.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManager.Backend.Entities
+{
+    public class Person : IdentityUser
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthdayData { get; set; }
+        public int Height { get; set; }
+        public int Weight { get; set; }
+        public Gender Gender { get; set; }
+        public bool IsAdmin { get; set; } = false;
+        public List<Image> Images { get; set; }
+        public enum Gender
+        {
+            Male,
+            Female,
+            Child
+        }
+    }
+}
---
+++ StudentManager.Model/Entities/Training.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManager.Backend.Entities
+{
+    public class Training
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public List<Exercise> Exercise { get; set; }
+        public int Duration { get; set; }
+        public FitnessProgram FintessProgram { get; set; }
+        public int FitnessProgramId { get; set; }
+    }
+}
